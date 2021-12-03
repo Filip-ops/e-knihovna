8ed0ca074e35b5468e830dee167eb75ef73d39ad
filip <f.sapak17@gmail.com>
2021-12-03 15:53:32 +0100

    pls

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask import render_template, url_for, flash, redirect, request
-#import app, db, bcrypt
+import app#, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
 #from models import //////////// ADD MODELS
 #from forms import //////////// ADD FORMS
