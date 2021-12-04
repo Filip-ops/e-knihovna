3f1238dd4d9da1cb83e50cccd002d37cca52797f
20putalam <marek.putala@email.cz>
2021-12-04 14:41:24 +0100

    changes were made

---
+++ flasklib/static/add_note.js
@@ -0,0 +1,13 @@
+let modal_note = document.getElementById("add-note-modal");
+
+let btn_note = document.getElementById("add-note-btn");
+
+let close_note = document.getElementById("add-note-close");
+
+btn_note.onclick = function() {
+  modal_note.style.display = "block";
+}
+
+close_note.onclick = function() {
+  modal_note.style.display = "none";
+}
