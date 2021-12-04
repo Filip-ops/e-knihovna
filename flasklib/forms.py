16c291af3f1d5eb06229c0fe8b419e4c6445f7ae
20putalam <62297465+20putalam@users.noreply.github.com>
2021-12-04 14:52:47 +0100

    deleted fomrs

--- flasklib/forms.py
+++ flasklib/forms.py
@@ -3,7 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
 from wtforms.fields.numeric import IntegerField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from flasklib.models import Book, Library, User
+from flasklib.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
