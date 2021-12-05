4fc3cc339e13aefb4482c399faaef589d71817c9
xdudaj02 <xdudaj02@stud.fit.vutbr.cz>
2021-12-05 17:47:21 +0100

    requirements update

--- flasklib/__init__.py
+++ flasklib/__init__.py
@@ -3,14 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_admin.contrib.sqla import ModelView
-from flask_admin import Admin,AdminIndexView
+from flask_admin import Admin, AdminIndexView
 from flask_login import UserMixin
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vatqztvxwsroxt:59ef0b95be0a73506825730a56774113565ef8451273319ec6963ac22a00ef71@ec2-52-17-1-206.eu-west-1.compute.amazonaws.com:5432/das19305gveklt'
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'postgresql://vatqztvxwsroxt:59ef0b95be0a73506825730a56774113565ef8451273319ec6963ac22a00ef71@ec2-52-17-1-206.eu-west-1.compute.amazonaws.com:5432/das19305gveklt'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
@@ -18,10 +21,13 @@ login_manager.login_message_category = 'info'
 
 admin = Admin(app)
 
+
 class MyModelView(ModelView):
     pass
 
+
 class MyAdminIndexView(AdminIndexView):
     pass
+
 
 from flasklib import routes
