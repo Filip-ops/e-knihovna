d7b90b885508c0242ff6e28dcfa40f425d73dca2
filip <f.sapak17@gmail.com>
2022-01-23 02:34:43 +0100

    fix

--- flasklib/routes.py
+++ flasklib/routes.py
@@ -530,7 +530,7 @@ def showTitle(id):
                                     library_title=lib_title.id)
                         db.session.add(note)
                         db.session.commit()
-                        idn = note.id;
+                        idn = note.id
                         data = {'name': name, 'start_page': start_page, 'end_page': end_page, 'text': text, 'color': color, 'idn': idn}
                         return make_response(jsonify(data))
                     
