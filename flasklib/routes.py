5769ecca03862222ab577a234c6f6a40960255a1
filip <f.sapak17@gmail.com>
2022-01-23 03:23:57 +0100

    x

--- flasklib/routes.py
+++ flasklib/routes.py
@@ -533,7 +533,7 @@ def showTitle(id):
                         db.session.commit()
                         idn = note.id
                         data = {'name': name, 'start_page': start_page, 'end_page': end_page, 'text': text, 'color': color, 'idn': idn}
-                        return make_response(jsonify(data))
+                        return make_response(jsonify(data),200)
                     
 
 
